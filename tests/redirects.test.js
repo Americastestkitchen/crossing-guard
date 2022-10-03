8fcf9e82dc42e5dded8f04ffeb81eb2131a6695c
Michael Ng <michaelng270@gmail.com>
2022-10-03 11:28:42 -0400

    Update redirects test for cleaner output

--- tests/redirects.test.js
+++ tests/redirects.test.js
@@ -1,8 +1,14 @@
 const fs = require('fs');
+require('dotenv').config();
+
+let resultData = 'Redirect Test Results';
 
 // Return a response from the given URL
 const getResponse = async (url) => {
-  const response = await fetch(url);
+  const headers = new Headers();
+  const basicAuthCreds = `${process.env.BASIC_USERNAME}:${process.env.BASIC_PASSWORD}`;
+  headers.set('Authorization', `Basic ${Buffer.from(basicAuthCreds, 'utf-8').toString('base64')}`);
+  const response = await fetch(url, { method: 'GET', headers });
   const responseAsHTML = await response.text();
 
   return {
@@ -22,10 +28,24 @@ describe('URLs should redirect', () => {
     const fileURLs = UrlArray[i].split(',');
     const initialURL = fileURLs[0];
     const expectedURL = fileURLs[1];
-    test(`"${initialURL}" should redirect to "${expectedURL}"`, async () => {
+    const testName = `"${initialURL}" should redirect to "${expectedURL}"`;
+    test(testName, async () => {
       const response = await getResponse(initialURL);
+      console.log(testName);
+      if (response.status !== 200 || response.finalResponse !== expectedURL) {
+        resultData += `\n\n${testName}: \n${response.status} ${response.finalResponse === expectedURL}`;
+      }
+      // Jest test expects
       expect(response.status).toBe(200);
       expect(response.finalResponse).toBe(expectedURL);
     });
   }
+});
+
+// Compile results into a file after all tests have run
+afterAll(() => {
+  const currentDate = new Date();
+  fs.appendFile(`test-result-${currentDate.getTime()}.txt`, resultData, (error) => {
+    if (error) console.warn(error);
+  });
 });
